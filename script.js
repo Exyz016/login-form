406ba7a286b57cfa410fbb66e2e16eb05819d51b
EzOk016 <joewin0504@student.grillska.se>
2025-10-15 20:28:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -69,10 +69,9 @@ async function handleLogin(event) {
 
     if (user) {
         document.getElementById('loginForm').reset(); // Reset login form
-        window.location.href = "KoZe-Playground/index.html";
+        window.location.href = "https://ezok016.github.io/login-form/KoZe-Playground/index.html";
     } else {
         showMessage("Invalid login details", "error");
     }
 
 }
-
